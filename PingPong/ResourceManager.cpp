4b88199d10550ad2ee83350f4f672ae0eb4e4c5b
LordNani <47566320+LordNani@users.noreply.github.com>
2019-06-23 16:59:45 +0300

    prototype(not tested) Resource mngr

---
+++ PingPong/ResourceManager.cpp
@@ -0,0 +1,58 @@
+#include "ResourceManager.h"
+
+sf::Texture* ResourceManager::RequestTexture(std::string texId) {
+	if (m_Textures[texId]) return m_Textures[texId];
+
+	return nullptr;
+}
+sf::Image* ResourceManager::RequestImage(std::string imgId) {
+	if (m_Images[imgId]) return m_Images[imgId];
+
+	return nullptr;
+}
+
+bool ResourceManager::addTextureFromFile(const std::string fileName, const std::string texId) {
+	sf::Texture *tex;
+	if (!tex->loadFromFile("assets\\" + fileName))
+		return false;
+	m_Textures.insert(std::pair< std::string, sf::Texture*>(texId,tex));
+	return true;
+}
+
+bool ResourceManager::addImageFromFile(const std::string fileName, const std::string imgId) {
+	sf::Image* img;
+	if (!img->loadFromFile("assets\\" + fileName))
+		return false;
+	m_Images.insert(std::pair< std::string, sf::Image*>(imgId, img));
+	return true;
+}
+
+bool ResourceManager::addTextureFromImage(sf::Image &sourceImg, const std::string texId) {
+	sf::Texture* tex;
+	if(!tex->loadFromImage(sourceImg))
+		return false;
+	m_Textures.insert(std::pair< std::string, sf::Texture*>(texId, tex));
+	return true;
+}
+
+bool ResourceManager::addImageFromColor( unsigned int width, unsigned int  height, sf::Color &color,std::string imgId) {
+	sf::Image* img;
+	sf::Image test;
+	img->create(width, height, color);
+	m_Images.insert(std::pair< std::string, sf::Image*>(imgId, img));
+}
+
+void ResourceManager::clearTextures() {
+	for (auto& i : m_Textures) {
+		delete i.second;
+		i.second = nullptr;
+	}
+}
+void ResourceManager::clearImages() {
+	for (auto& i : m_Images) {
+		delete i.second;
+		i.second = nullptr;
+	}
+}
+
+ResourceManager::~ResourceManager() {}
---
+++ PingPong/ResourceManager.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <map>
+#include <SFML/Graphics.hpp>
+class ResourceManager {
+public:
+	static ResourceManager* getInstance() {
+		static ResourceManager instance;
+		return &instance;
+	}
+	~ResourceManager();
+	void clearTextures();
+	void clearImages();
+	sf::Texture* RequestTexture(std::string texId);
+	sf::Image* RequestImage(std::string imgId);
+	bool addTextureFromFile(const std::string fileName, const std::string texId);
+	bool addImageFromFile(const std::string fileName, const std::string imgId);
+	bool addTextureFromImage(sf::Image &sourceImg, const std::string texId);
+	bool addImageFromColor(unsigned int width, unsigned int  height, sf::Color& color, std::string imgId);
+private:
+	std::map<std::string, sf::Texture*> m_Textures;
+	std::map<std::string, sf::Image*> m_Images;
+};
